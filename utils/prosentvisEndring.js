75c0fa734eb7cff899bef25149da78c3f990b3e2
MatNygaard <mathias.nygaard1@gmail.com>
2025-05-12 01:52:33 +0200

    unittest

--- utils/prosentvisEndring.js
+++ utils/prosentvisEndring.js
@@ -1,6 +1,7 @@
+//Beregner prosentvis endring mellom to tall
 function prosentvisEndring(ny, gammel) {
-    if (gammel === 0) return 0;
-    return parseFloat ((((ny - gammel) / gammel)* 100).toFixed(2));
+    if (gammel === 0) return 0; //unngår deling på null
+    return parseFloat ((((ny - gammel) / gammel)* 100).toFixed(2)); //returnerer avrundet prosentvis endring
 }
 
 module.exports = { prosentvisEndring };
--- utils/validering.js
+++ utils/validering.js
@@ -1,3 +1,4 @@
+//sjekker om ISIN kode er gyldig, må være en streng og 12 tegn
 function gyldigISIN(isin){
     return typeof isin ==='string' && isin.length === 12;
 }
